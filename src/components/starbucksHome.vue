9016b5b616882421f491679a1ed93073bfc4caf0
dolatjangir <dolatjangir778@gmail.com>
2024-03-01 21:37:55 +0530

    first commit

---
+++ src/components/starbucksHome.vue
@@ -0,0 +1,493 @@
+<template>
+<div class="outer-container">
+
+    <div class="inner-container">
+        <div class="header">
+            <div class="left">
+                <img class="starbucks-img" src="https://www.starbucks.in/assets/icon/logo.png" />
+                <button class="menu-button">MENU</button>
+                <button class="rewards-button">REWARD</button>
+                <button>GIFT CARDS</button>
+            </div>
+            <div class="right">
+                <button><i class="fa-solid fa-location-dot"></i> Find Store</button>
+                <button class="sign-in">Sign In</button>
+                <button class="join-now">Join Now</button>
+            </div>
+        </div>
+        <div class="lower-header">
+            <p class="starbucks-rewards">STARBUCKS® REWARDS</p>
+        </div>
+        <div class="photo-page">
+            <div class="photo-page-text">
+                <h2 class="free-coffee">FREE COFFEE <br> IS A TAP AWAY</h2>
+                <p><span>Join now to start earning Rewards.</span></p>
+                <button class="joinbutton">join now</button>
+                <span class="for-join-app">
+                    Or <a class="color" href="https://www.starbucks.com/rewards/mobile-apps">
+                        join in the app</a> for the best experience</span>
+            </div>
+            <div class="star-img">
+                <img src="../assets/starbucks-img.png" alt="star-img" />
+            </div>
+        </div>
+        <div class="page-2">
+            <div class="header-page-2">
+                <h1>getting started is easy</h1>
+                <p>Earn stars and get rewards in a few easy steps.</p>
+            </div>
+            <div class="virtical-div">
+                <div class="numbers">
+                    <div class="card-img">
+                        <img class="number-img" src="	https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-1@2x.jpg" />
+                    </div>
+                    <div class="card-content">
+                        <h3>create a account</h3>
+                        <p>To get started,<a href="#">join now.</a>You can also<a href="#">join in the app</a>to get access to the full range of Starbucks® Rewards benefits.</p>
+                    </div>
+                </div>
+                <div class="numbers">
+                    <div class="card-img">
+                        <img class="number-img" src="https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-2@2x.jpg" />
+                    </div>
+                    <div class="card-content">
+                        <h3>Order and pay how you’d like</h3>
+                        <P>Use cash, credit/debit card or save some time and pay right through the app. You’ll collect Stars all ways.<a href="#">Learn now</a></P>
+                    </div>
+                </div>
+                <div class="numbers">
+                    <div class="card-img">
+                        <img class="number-img" src="	https://www.starbucks.com/weblx/images/rewards/getting-started/getting-started-3@2x.jpg" />
+                    </div>
+                    <div class="card-content">
+                        <h3>Earn Stars, get Rewards</h3>
+                        <p>As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars!</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="page-3">
+            <h1 class="heading-page-3">Get your favorites for free</h1>
+        </div>
+        <div class="page-4">
+            <div class="buttonswithstar">
+                <button class="page-4-button1"><span class="buttonnumber">25</span><span class="starofbutton">★</span></button>
+                <button class="page-4-button"><span class="buttonnumber">100</span><span class="starofbutton">★</span></button>
+                <button class="page-4-button"><span class="buttonnumber">200</span><span class="starofbutton">★</span></button>
+                <button class="page-4-button"><span class="buttonnumber">300</span><span class="starofbutton">★</span></button>
+                <button class="page-4-button"><span class="buttonnumber">400</span><span class="starofbutton">★</span></button>
+            </div>
+            <div class="buttonclickimg">
+                <img class="coffeemugimg" src="https://www.starbucks.com/weblx/images/rewards/reward-tiers/025.png" alt="coffeeMug" />
+                <div class="img-detail">
+                    <h3 class="h-teg">Customize your drink</h3>
+                    <p class="mug-detail">Make your drink just right with an extra espresso<br /> shot, nondairy milk or a dash of your favorite<br /> syrup.</p>
+                </div>
+            </div>
+        </div>
+        <div class="page-5">
+            <div class="page-5-header">
+                <h1>Endless Extras</h1>
+                <p>Joining Starbucks® Rewards means unlocking access to exclusive benefits. Say hello to<br />easy ordering, tasty Rewards and—yes, free coffee.</p>
+            </div>
+            <div class="page-5-footer">
+                <div class="page-5-cards">
+                    <img class="page-5-img" src="	https://www.starbucks.com/weblx/images/rewards/benefits/1-fun-freebies.jpg" alt="card-1">
+                    <h2 class="page-5-hteg">Fun freebies</h2>
+                    <p>Not only can you earn free coffee, look forward to<br />a birthday treat plus coffee and tea refills.<br /><br /><a href="#">Learn more</a></p>
+                </div>
+                <div class="page-5-cards">
+                    <img class="page-5-img" src="https://www.starbucks.com/weblx/images/rewards/benefits/2-order-and-pay-ahead.jpg" alt="card-2">
+                    <h2 class="page-5-hteg">Order & pay ahead</h2>
+                    <p>Enjoy the convenience of in-store, curbside or<br />drive-thru pickup at select stores.<br /><br /><a href="#">Learn more</a></p>
+                </div>
+                <div class="page-5-cards">
+                    <img class="page-5-img" src="	https://www.starbucks.com/weblx/images/rewards/benefits/3-get-to-free-faster.jpg" alt="card-3">
+                    <h2 class="page-5-hteg">Get to free faster</h2>
+                    <p>Earn Stars even quicker with Bonus Star<br />challenges, Double Star Days and exciting<br />games.<br /><br /><a href="#">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="page-6">
+            <div class="page-6-header">
+                <h1>Cash or card, you earn Stars</h1>
+                <p>No matter how you pay, you can earn Stars with your morning coffee. Those Stars <br />add up to (really delicious) Rewards.</p>
+            </div>
+            <div class="page-6-footer">
+                <div class="page-6-card-1">
+                    <h1>1★Star per dollar</h1>
+                    <p>Pay as you go</p>
+                </div>
+                <div class="page-6-card-2">
+                    <img class="page-6-img" src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1A.png" alt="phone-qr-img" />
+                    <div class="page-6-card-2-p">
+                        <h2>Scan and pay separately</h2>
+                        <p class="page-6-pteg">Use cash or credit/debit card at<br /> the register.</p>
+                    </div>
+                </div>
+                <div class="page-6-card-3">
+                    <img class="page-6-img" src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1B.png" alt="payment-img" />
+                    <div class="page-6-card-3-p">
+                        <h2>Save payment in the app</h2>
+                        <p class="page-6-pteg">Check-out faster by saving a<br /> credit/debit card or PayPal to<br /> your account. You’ll be able to<br /> order ahead or scan and pay at<br /> the register in one step.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- this is page-7 but page-6 is similar of page-7 then i copy page-6 for save time -->
+        <!--thsi is page-7 -->
+        <div class="page-7">
+
+            <div class="page-6-footer">
+                <div class="page-6-card-1">
+                    <h1>2★Stars per dollar</h1>
+                    <p>Add funds in the app</p>
+                </div>
+                <div class="page-6-card-2">
+                    <img class="page-6-img" src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2A.png" alt="dollar-img" />
+                    <div class="page-6-card-2-p">
+                        <h2>Preload</h2>
+                        <p class="page-6-pteg">To save time and earn Stars<br /> twice as fast, add money to your
+                            <br />digital Starbucks Card using any <br />payment option. Scan and pay in
+                            <br /> one step or order ahead in the<br />app. </p>
+                    </div>
+                </div>
+                <div class="page-6-card-3">
+                    <img class="page-6-img" src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/2B.png" alt="gift-card-img" />
+                    <div class="page-6-card-3-p">
+                        <h2>Register your gift card</h2>
+                        <p class="page-6-pteg">Then use it to pay through the<br /> app. You can even consolidate<br />one place.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "starbucksHomePage"
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.outer-container {
+    width: 100%;
+
+}
+
+.inner-container {
+    border: 1px solid #fff;
+
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    height: 80px;
+
+}
+
+.left {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+}
+.rewards-button{
+    border: none;
+    height: 77px;
+    border-bottom: 5px solid #008248;
+}
+.right {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+}
+
+.join-now {
+    border: 1px solid black;
+    border-radius: 20px;
+    background-color: black;
+    color: white;
+    width: 86px;
+}
+.sign-in{
+    border: 1px solid black;
+    border-radius: 19px;
+    width: 79px;
+}
+.starbucks-img {
+    width: 53px;
+    height: 42px;
+    position: relative;
+    left: 20px;
+
+}
+
+button {
+    width: 120px;
+    height: 30px;
+    font-size: unset;
+    border: none;
+    background-color: transparent;
+}
+
+.lower-header {
+    position: sticky;
+    top: 0px;
+    width: 100%;
+    /* Or whatever width you desire */
+    z-index: 1;
+    /* Adjust z-index as needed */
+    /* background-color: #fff;  */
+    background-color: #1e3932;
+    height: 45px;
+    display: flex;
+    align-items: center;
+
+}
+
+.starbucks-rewards {
+    font-family: sans-serif;
+    position: relative;
+    left: 150px;
+    color: #fff;
+    word-spacing: 5px;
+    font-weight: 500;
+
+}
+
+.photo-page-text {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    left: 50px;
+    gap: 20px;
+    font-family: sans-serif;
+}
+
+.photo-page {
+    display: flex;
+    flex-direction: row;
+    background-color: #d4e9e2;
+}
+
+.star-img {
+    position: relative;
+    right: 28%;
+
+}
+
+img {
+    position: relative;
+    width: 187%;
+    height: 100%;
+}
+
+.joinbutton {
+    word-spacing: 7px;
+    font-family: math;
+    font-weight: 600;
+    height: 34px;
+    border: none;
+    /* border: 1px solid red; */
+    border-radius: 45px;
+    color: #fff;
+    background-color: #008248;
+}
+
+a {
+    color: black;
+}
+
+.number-img {
+    width: 50px;
+    height: 50px;
+}
+
+.page-2 {
+    display: flex;
+    flex-direction: column;
+    margin: 65px;
+    padding: 10px;
+
+}
+
+.header-page-2 {
+    padding-left: 30%;
+    padding-bottom: 41px;
+}
+
+.virtical-div {
+    display: flex;
+}
+
+.card-img {
+    margin: 10px;
+    padding: 0 0 0 87px;
+}
+
+p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 4px;
+    font-family: sans-serif;
+    font-size: large;
+    word-spacing: 2px;
+}
+
+.heading-page-3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f1f8f6;
+    height: 143px;
+}
+
+.page-4 {
+    display: flex;
+    flex-direction: column;
+    background-color: #f1f8f6;
+    font-family: inherit;
+}
+
+.buttonswithstar {
+
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+
+.buttonnumber {
+    font-weight: 600 !important;
+    font-size: x-large;
+}
+
+.starofbutton {
+    font-size: large;
+    color: #cba258;
+}
+
+.page-4-button1 {
+    border-bottom: 5px solid #008248;
+    cursor: pointer;
+
+}
+
+.coffeemugimg {
+    height: auto;
+    max-width: 40%;
+}
+
+.buttonclickimg {
+    display: flex;
+    align-items: center;
+    background-color: #d4e9e2;
+
+}
+
+.page-5-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: inherit;
+    padding-top: 100px;
+    padding-bottom: 50px;
+}
+
+.page-5-img {
+    height: 109px;
+    width: 112px;
+}
+
+.page-5-footer {
+    display: flex;
+    justify-content: space-around;
+    padding-bottom: 100px;
+}
+
+.page-5-hteg {
+    margin: 10px;
+}
+
+.page-5-img {
+    padding-left: 40px;
+}
+
+.page-6-img {
+    width: 22%;
+    /* height: 50%; */
+}
+
+.page-6-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 7%;
+    padding-bottom: 7%;
+}
+
+.page-6-footer {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    /* justify-content: space-between;
+    align-items: center; */
+}
+
+.page-6-card-1 {
+    font-size: small;
+    padding-right: 70px;
+}
+
+.page-6-card-2,
+.page-6-card-3 {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+}
+
+.page-6 {
+    background-color: #f3f1e7;
+    border: none;
+    border-bottom: 1px solid #1e3932;
+}
+
+.page-6-card-2-p,
+.page-6-card-3-p {
+    display: flex;
+    flex-direction: column;
+}
+
+.page-6-pteg {
+    font-size: 90%;
+}
+
+.page-7 {
+    background-color: #f3f1e7;
+    border: none;
+    padding-top: 6%;
+    padding-bottom: 4%;
+}
+</style>
